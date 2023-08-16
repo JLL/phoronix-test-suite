992900f98f4abc81ef92806fc79721f1ea224e62
Jean-Louis Ly <jeanlouis.ly@gmail.com>
2023-08-16 21:28:13 +0900

    pmbench aarch64 support

--- ob-cache/test-profiles/pts/pmbench-1.0.2/install.sh
+++ ob-cache/test-profiles/pts/pmbench-1.0.2/install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-tar -xf jisooy-pmbench-46a3d394ca7b.tar.xz
-cd jisooy-pmbench-46a3d394ca7b
-make pmbench
+tar -xzf hesham-huawei-pmbench-1ea99f9b24cd.tar.gz
+cd hesham-huawei-pmbench-1ea99f9b24cd
+make pmbench march=aarch64
 echo $? > ~/install-exit-status
 
 cd ~
 echo "#!/bin/bash
-cd jisooy-pmbench-46a3d394ca7b
+cd hesham-huawei-pmbench-1ea99f9b24cd
 ./pmbench \$@ 60 > \$LOG_FILE 2>&1" > pmbench
 chmod +x pmbench
